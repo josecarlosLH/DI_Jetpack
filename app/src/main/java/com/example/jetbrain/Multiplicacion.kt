fa3b87e531d85eeef34d3efb12d5042986a8785e
josecarlosLH <micheldemotril99@gmail.com>
2021-02-18 14:27:57 +0100

    v1.01: Adding missing files

---
+++ app/src/main/java/com/example/jetbrain/Multiplicacion.kt
@@ -0,0 +1,31 @@
+package com.example.practicajetpack
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_sum.*
+
+class Multplicacion : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_multiplication, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            val resultado = MultplicacionArgs.fromBundle(it).resultado;
+            Resultado.setText("La Multiplicacion es "+resultado.toString())
+        }
+    }
+}
